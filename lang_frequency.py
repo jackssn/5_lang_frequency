5d8c0bedb75bf531c32a7b2c535e89d85408accb
Jack <mtjeck@gmail.com>
2016-09-30 01:15:31 +0300

    Code finished

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,26 @@
+import os
+import string
+import operator
+from collections import Counter
+from pprint import pprint
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath,'r') as f:
+        return f.read()
 
 
 def get_most_frequent_words(text):
-    pass
-
+    words = [word.strip(string.punctuation) for word in text.split()]
+    counts = Counter(words)
+    return sorted(counts.items(), key=operator.itemgetter(1), reverse=True)[:10]
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Type full path to file:\n')
+    text = load_data(filepath)
+    if text:
+        pprint(get_most_frequent_words(text))
+    else:
+        print('File not found')
